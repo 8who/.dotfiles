a1f721a4084613b937d327b74f55cd51caab96ce
8who <who.0x8@gmail.com>
2025-08-31 21:12:08 +0200

    #2 Deleted: outdated and confusing nvim-dap config

--- .config/nvim/after/ftplugin/c.lua
+++ .config/nvim/after/ftplugin/c.lua
@@ -36,36 +36,3 @@ cmp.setup.filetype("c", {
         { name = "path" },
     }),
 })
-
--- Fast compile and flash keybindings
-local keymap = vim.keymap.set
-keymap("n", "<leader>mb", ":make<CR>", { noremap = true, silent = true, desc = "Build Project" })
-keymap("n", "<leader>mc", ":make clean<CR>", { noremap = true, silent = true, desc = "Clean Build" })
-keymap(
-    "n",
-    "<leader>mf",
-    ":TermExec cmd='st-flash write build/your_program.bin 0x8000000'<CR>",
-    { noremap = true, silent = true, desc = "Flash Firmware" }
-)
-keymap(
-    "n",
-    "<leader>ms",
-    ":TermExec cmd='minicom -D /dev/ttyACM0 -b 115200'<CR>",
-    { noremap = true, silent = true, desc = "Serial Monitor" }
-)
-
--- DAP debugging keymaps
-keymap(
-    "n",
-    "<leader>db",
-    ":lua require'dap'.toggle_breakpoint()<CR>",
-    { noremap = true, silent = true, desc = "Toggle Breakpoint" }
-)
-keymap("n", "<leader>dc", ":lua require'dap'.continue()<CR>", { noremap = true, silent = true, desc = "Continue" })
-keymap("n", "<leader>ds", ":lua require'dap'.step_over()<CR>", { noremap = true, silent = true, desc = "Step Over" })
-keymap("n", "<leader>di", ":lua require'dap'.step_into()<CR>", { noremap = true, silent = true, desc = "Step Into" })
-keymap("n", "<leader>do", ":lua require'dap'.step_out()<CR>", { noremap = true, silent = true, desc = "Step Out" })
-keymap("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>", { noremap = true, silent = true, desc = "Open REPL" })
-keymap("n", "<leader>dl", ":lua require'dap'.run_last()<CR>", { noremap = true, silent = true, desc = "Run Last" })
-keymap("n", "<leader>du", ":lua require'dapui'.toggle()<CR>", { noremap = true, silent = true, desc = "Toggle UI" })
-keymap("n", "<leader>dt", ":lua require'dap'.terminate()<CR>", { noremap = true, silent = true, desc = "Terminate" })
